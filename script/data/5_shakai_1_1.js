ce6816a82820500efece22db76a0e00936614d1f
mochimochi315 <mochi213we@yahoo.co.jp>
2024-09-03 20:41:44 +0900

    20240903

--- script/data/5_shakai_1_1.js
+++ script/data/5_shakai_1_1.js
@@ -1,12 +1,12 @@
 const mondai = [
-    { number: 1, question: '問題１', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 2, question: '問題３', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 3, question: '問題６', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 4, question: '問題７', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 5, question: '問題８', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 6, question: '問題１２', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 7, question: '問題１４', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 8, question: '問題１５', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 9, question: '問題１６', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' },
-    { number: 10, question: '問題１９', answer: '答え', image_name: '1-shakai-1-01.png', etc_1: '', etc_2: '', etc_3: '', etc_4: '' }
+    { number: 1, question: 'P89のエを見て答えましょう。<BR>北からは、親潮と呼ばれる（　　）が流れてきています。', answer: 'かんりゅう', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: 'P89のエを見て答えましょう。<BR>南からは、黒潮と呼ばれる（　　）が流れてきています。', answer: 'だんりゅう', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 3, question: 'P88。日本の周りの海には、海流や地形のえいきょうで多くの種類の（　　　）が集まります。', answer: 'ぎょかいるい', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 4, question: 'P88。日本の全国各地には、2800近くの（　　　）がある。', answer: 'ぎょこう', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 5, question: 'P92。船に積んでいる魚を漁港にあげることを（　　　）という。', answer: 'みずあげ', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: 'P92。多くの人で競争をして、魚のねだんを決めていく方法を（　　　）という。', answer: 'せり', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 7, question: 'P92。せりでは、水産物の状態や水あげ量をみて、（　　　）が決められている。', answer: 'ねだん', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 8, question: 'P98のエを見て答えましょう。<BR>1980年と比べると、2020年の輸入量は、（　　　　いる）。', answer: 'ふえている', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 9, question: 'P100。世界各国は、自国の水産資源物を守るために、<BR>海岸から200海里のはん囲の海で、<BR>他国の漁船がとる魚の種類や量を（　　　）するようになりました。', answer: 'せいげん', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 10, question: 'P100のアを見て答えましょう。<BR>2017年の世界の水産物消費量は、1980年と比べて（　　　いる）。', answer: 'ふえている', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''}
 ];
