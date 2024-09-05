27a16df3846e7951ba63c422e5bf48d3fc437747
mochimochi315 <mochi213we@yahoo.co.jp>
2024-09-06 04:40:32 +0900

    20240906

---
+++ script/data/5_kokugo.js
@@ -0,0 +1,8 @@
+const tangen_mei= [
+'大造じいさんとがん'
+];
+
+//単元名をタグに書き込む。配列は、0から始まるので、「i-1」にしてある。
+for (let i = 1; i <= tangen_mei.length; i++) {
+document.getElementById('tangen_mei_' + i).textContent = tangen_mei[(i - 1)];
+}
