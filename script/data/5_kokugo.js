502700ca82bf4a9306aa4aa6cc98d28b639bcf4c
mochimochi315 <mochi213we@yahoo.co.jp>
2024-09-08 23:03:17 +0900

    Delete script/data/5_kokugo.js

--- script/data/5_kokugo.js
+++
@@ -1,8 +0,0 @@
-const tangen_mei= [
-'大造じいさんとがん','言葉の文化２'
-];
-
-//単元名をタグに書き込む。配列は、0から始まるので、「i-1」にしてある。
-for (let i = 1; i <= tangen_mei.length; i++) {
-document.getElementById('tangen_mei_' + i).textContent = tangen_mei[(i - 1)];
-}
