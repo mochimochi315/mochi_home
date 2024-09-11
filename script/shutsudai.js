8119fade508b65fbaa552e8b94df7256e03c2f46
mochimochi315 <mochi213we@yahoo.co.jp>
2024-09-11 12:34:49 +0900

    Add files via upload

--- script/shutsudai.js
+++ script/shutsudai.js
@@ -29,20 +29,20 @@ let mondai_hantei = [
     { number: 7, hantei: "no" },
     { number: 8, hantei: "no" },
     { number: 9, hantei: "no" },
-    { number: 10, hantei: "no" }
-    { number: 11, hantei: "no" }
-    { number: 12, hantei: "no" }
-    { number: 13, hantei: "no" }
-    { number: 14, hantei: "no" }
-    { number: 15, hantei: "no" }
-    { number: 16, hantei: "no" }
-    { number: 17, hantei: "no" }
-    { number: 18, hantei: "no" }
-    { number: 19, hantei: "no" }
+    { number: 10, hantei: "no" },
+    { number: 11, hantei: "no" },
+    { number: 12, hantei: "no" },
+    { number: 13, hantei: "no" },
+    { number: 14, hantei: "no" },
+    { number: 15, hantei: "no" },
+    { number: 16, hantei: "no" },
+    { number: 17, hantei: "no" },
+    { number: 18, hantei: "no" },
+    { number: 19, hantei: "no" },
     { number: 20, hantei: "no" }
 ];
 
-console.log(mondai_hantei);
+//console.log(mondai_hantei);
 
 // 以下は、コールバック関数の書式。'5_shakai_1_1.js'を読み込んで、完了後にrenderChartを実行
 //「loadScript」関数は、「'script/data/' + query_data8 + '?v=' + new Date().getTime()」を引数として渡し、
