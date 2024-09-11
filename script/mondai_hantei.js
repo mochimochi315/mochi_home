f57c75aad87d34fffccda164324610b280e34030
mochimochi315 <mochi213we@yahoo.co.jp>
2024-09-11 19:01:20 +0900

    20240911

---
+++ script/mondai_hantei.js
@@ -0,0 +1,26 @@
+//このファイルの中に、「'use strict';」を記述するとエラーになる。
+
+let mondai_hantei = [
+    { number: 1, hantei: "no" },
+    { number: 2, hantei: "no" },
+    { number: 3, hantei: "no" },
+    { number: 4, hantei: "no" },
+    { number: 5, hantei: "no" },
+    { number: 6, hantei: "no" },
+    { number: 7, hantei: "no" },
+    { number: 8, hantei: "no" },
+    { number: 9, hantei: "no" },
+    { number: 10, hantei: "no" },
+    { number: 11, hantei: "no" },
+    { number: 12, hantei: "no" },
+    { number: 13, hantei: "no" },
+    { number: 14, hantei: "no" },
+    { number: 15, hantei: "no" },
+    { number: 16, hantei: "no" },
+    { number: 17, hantei: "no" },
+    { number: 18, hantei: "no" },
+    { number: 19, hantei: "no" },
+    { number: 20, hantei: "no" }
+];
+
+
