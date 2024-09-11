9e8e2f2330a9e7351b4e736a47e61345678fe560
mochimochi315 <mochi213we@yahoo.co.jp>
2024-09-11 11:27:48 +0900

    Update 5_kokugo_daizou_11.js

--- script/data/5_kokugo_daizou_11.js
+++ script/data/5_kokugo_daizou_11.js
@@ -4,7 +4,7 @@ const mondai = [
     { number: 3, question: 'P95。なぜ、じいさんは、猟銃（りょうじゅう）を打たずに、もう少ししんぼうしたのですか。<BR>猟銃の（　　　）がまだとどかないから。', answer: 'たま', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 4, question: 'P96。残雪は、なぜ、ぐっと急角度に方向を変えたのですか。<BR>「（　　　）には、近づかぬがよいぞ。」とかれの本能がそう感じたから。', answer: 'ようすのかわったところ', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 5, question: 'P96。じいさんがつかまえたがんは、どこにいましたか。', answer: 'にわとりごや', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 6, question: 'P96。じいさんは、つかまえたがんを何年間育てましたか。', answer: 'にねんかん', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: 'P96。じいさんは、つかまえたがんを何年間育てましたか。', answer: '2ねんかん', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 7, question: 'P96。じいさんは、おとりのがんをつかって、何をとらえてやろうとおもったのですか。', answer: 'ざんせつのなかま', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 8, question: 'P98。じいさんは、なぜしばらく目をつぶったのですか。<BR>心の（　　　）のを待つため。', answer: 'おちつく', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 9, question: 'P99。はやぶさがやってきた時に、がんの群れは、どのように飛び去っていきましたか。<BR>実にすばやい動作で、（　　　ながら）飛び去っていった。', answer: 'はやぶさのめをくらましながら', image_name: '', etc_1: '', etc_2: '', etc_3: '', etc_4: ''},
