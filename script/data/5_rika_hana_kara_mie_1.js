0d52c6e664e1100498eda494ee0215f06f2d15ae
mochimochi315 <mochi213we@yahoo.co.jp>
2024-09-20 19:47:11 +0900

    Add files via upload

--- script/data/5_rika_hana_kara_mie_1.js
+++ script/data/5_rika_hana_kara_mie_1.js
@@ -1,6 +1,6 @@
 const mondai = [
-    { number: 1, question: 'この花の名前は？', answer: 'めばな', image_name: '5-rika-1-01.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 2, question: 'この花の名前は？', answer: 'おばな', image_name: '5-rika-1-02.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 1, question: 'これは、ヘチマです。この花の名前は？', answer: 'めばな', image_name: '5-rika-1-01.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: 'これは、ヘチマです。この花の名前は？', answer: 'おばな', image_name: '5-rika-1-02.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 3, question: 'Aの部分は、何といいますか？', answer: 'めしべ', image_name: '5-rika-1-03.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 4, question: 'Bの部分は、何といいますか？', answer: 'おしべ', image_name: '5-rika-1-04.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 5, question: 'これはどちらの花ですか？（めしべ、おしべ）', answer: 'めしべ', image_name: '5-rika-1-05.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
