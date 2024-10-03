ed572d6648c9a6c625d53d79b14ca1f24394f762
mochimochi315 <mochi213we@yahoo.co.jp>
2024-10-03 14:15:32 +0900

    Add files via upload

--- script/data/5nen/shakai/5_shakai_syokuryou_2.js
+++ script/data/5nen/shakai/5_shakai_syokuryou_2.js
@@ -4,7 +4,7 @@ const mondai = [
     { number: 3, question: 'P113。交通の発達や冷凍技術の進歩によって、（　　　）な食材を運べるようになった。', answer: 'しんせん', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 4, question: 'P114。限られた国からの輸入にたよりすぎると、それらの国で事故や災害があった場合に、食生活が（　　　）になる心配がある。', answer: 'ふあんてい', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 5, question: 'P116。生産からはん売までをすべて管理することで、（　　　）作物を安く確実にとどけることができる。', answer: 'しつのよい', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 6, question: 'P117。形や見ばえの悪い野菜を売ることで、すてられる（　　　う）の量を減らしている。', answer: 'しょくりょう', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: 'P117。形や見ばえの悪い野菜を売ることで、すてられる（　　　りょう）の量を減らしている。', answer: 'しょくりょう', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 7, question: 'P117。（　　　）では、地元でとれた農産物や、それらを材料にした加工品などもうっています。', answer: 'ちょくばいじょ', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 8, question: 'P113。食料自給率が100％なのは、（　　　）である。', answer: 'こめ', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 9, question: 'P113。自給率が下がったのは、輸入が増えただけでなく、国内の（　　　）が減ったことも原因の１つである。', answer: 'せいさん', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
