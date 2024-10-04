ce84002a2a62777a5650546e14ceca7a988cd0dd
mochimochi315 <mochi213we@yahoo.co.jp>
2024-10-04 12:56:00 +0900

    Add files via upload

---
+++ script/data/5nen/rika/5_rika_hito_no_tanjyou_4.js
@@ -0,0 +1,10 @@
+const mondai = [
+    { number: 1, question: 'これは、ヒトの（　　　）の画像です。', answer: 'せいし', image_name: '5-rika-hito_no_tanjyou-09.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: 'P92。約10週のヒトの子どもの身長は、約（　　　）㎝です。（半角数字で）', answer: '9', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 3, question: 'P93。約26週のヒトの子どもの身長は、約（　　　）㎝です。（半角数字で）', answer: '35', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 4, question: 'P93。約34週のヒトの子どもの身長は、約（　　　）㎝です。（半角数字で）', answer: '45', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 5, question: 'P92。約10週のヒトの子どもの体重は、約（　　　）gです。（半角数字で）', answer: '20', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: 'P93。約26週のヒトの子どもの体重は、約（　　　）gです。（半角数字で）', answer: '1000', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 7, question: 'P93。約34週のヒトの子どもの体重は、約（　　　）gです。（半角数字で）', answer: '2000', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 8, question: 'P99。周りの人に、おなかに赤ちゃんがいることを示すためのマークのことを（　　　）という。（カタカナで）', answer: 'マタニティマーク', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''}
+];
---
+++ script/data/5nen/rika/5_rika_hito_no_tanjyou_5.js
@@ -0,0 +1,9 @@
+const mondai = [
+    { number: 1, question: 'P92。約18週のヒトの子どもの身長は、約（　　　）㎝です。（半角数字で）', answer: '25', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: 'P92。約18週のヒトの子どもの体重は、約（　　　）gです。（半角数字で）', answer: '250', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 3, question: 'P92。約4週のヒトの子どもの身長は、約（　　　）㎝です。（半角数字で）', answer: '0.4', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 4, question: 'P92。約4週のヒトの子どもの体重は、約（　　　）gです。（半角数字で）', answer: '0.02', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 5, question: 'P92。これは、約（　　　）週頃の胎児（たいじ）の画像です。（半角数字で）', answer: '4', image_name: '5-rika-hito_no_tanjyou-10.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: 'P92。これは、約（　　　）週頃の胎児（たいじ）の画像です。（半角数字で）', answer: '7', image_name: '5-rika-hito_no_tanjyou-11.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 7, question: 'P92。これは、約（　　　）週頃の胎児（たいじ）の画像です。（半角数字で）', answer: '20', image_name: '5-rika-hito_no_tanjyou-12.png', answer2: '', etc_2: '', etc_3: '', etc_4: ''}
+];
