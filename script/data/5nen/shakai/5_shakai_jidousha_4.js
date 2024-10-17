dba3d19f599342d8d78003176d5c2704e150652d
mochimochi315 <mochi213we@yahoo.co.jp>
2024-10-17 09:20:13 +0900

    Update 5_shakai_jidousha_4.js

--- script/data/5nen/shakai/5_shakai_jidousha_4.js
+++ script/data/5nen/shakai/5_shakai_jidousha_4.js
@@ -4,7 +4,7 @@ const mondai = [
     { number: 3, question: 'P132。シート工場の検査では、少しでもしわやきずがあれば、すぐに（　　　）なくてはなりません。', answer: 'とりかえ', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 4, question: 'P133。１台の自動車を生産するためには、２万から３万個ほどの（　　　）が必要になります。', answer: 'ぶひん', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 5, question: 'P134。自動車工場から、遠い地域に運ばれる自動車は、工場のそばの港で、専用の（　　　）に積みこまれます。', answer: 'ふね', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 6, question: 'P135。集めた（　　　）をもとに、どのような自動車にしたいか、どの機能を取り入れるかを考えていきます。（カタカナ）', answer: 'ニーズ', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: 'P136。集めた（　　　）をもとに、どのような自動車にしたいか、どの機能を取り入れるかを考えていきます。（カタカナ）', answer: 'ニーズ', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 7, question: 'P137。駐車を助ける機能がついた車は、駐車場に駐車する時に（　　　）の操作（そうさ）などを自動でしてくれる。（カタカナ）', answer: 'ハンドル', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 8, question: 'P138。回転するシートがついた車は、（　　　）やベビーカーを使う人でも乗り降りがしやすくなっている。', answer: 'くるまいす', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 9, question: 'P138。（　　　）自動車は、バッテリーの電気で動く自動車です。環境にやさしい自動車といえます。', answer: 'でんき', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
