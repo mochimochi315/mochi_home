24c09359fa1d604382dabeb043ea5524d726897a
mochimochi315 <mochi213we@yahoo.co.jp>
2024-12-06 08:13:48 +0900

    Add files via upload

---
+++ script/data/sonota/sonota_2%1_1.js
@@ -0,0 +1,10 @@
+const mondai = [
+    { number: 1, question: '1÷1＝', answer: '1', image_name: '', answer2: '1', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: '18÷9＝', answer: '2', image_name: '', answer2: '2', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 3, question: '32÷8＝', answer: '4', image_name: '', answer2: '4', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 4, question: '42÷7＝', answer: '6', image_name: '', answer2: '6', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 5, question: '48÷6＝', answer: '8', image_name: '', answer2: '8', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 7, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 8, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''}
+];
---
+++ script/data/sonota/sonota_2%1_13.js
@@ -0,0 +1,9 @@
+const mondai = [
+    { number: 1, question: '8÷4＝', answer: '2', image_name: '', answer2: '2', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: '12÷3＝', answer: '4', image_name: '', answer2: '4', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 3, question: '12÷2＝', answer: '6', image_name: '', answer2: '6', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 4, question: '8÷1＝', answer: '8', image_name: '', answer2: '8', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 5, question: '81÷9＝', answer: '9', image_name: '', answer2: '9', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 7, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''}
+];
