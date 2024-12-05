31d3c8be633a72a0cc2bfff19bdcab7206701349
mochimochi315 <mochi213we@yahoo.co.jp>
2024-12-06 08:14:55 +0900

    Add files via upload

---
+++ script/data/sonota/sonota_2%1_3.js
@@ -0,0 +1,10 @@
+const mondai = [
+    { number: 1, question: '3÷3＝', answer: '1', image_name: '', answer2: '1', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: '6÷2＝', answer: '3', image_name: '', answer2: '3', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 3, question: '5÷1＝', answer: '5', image_name: '', answer2: '5', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 4, question: '54÷9＝', answer: '6', image_name: '', answer2: '6', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 5, question: '64÷8＝', answer: '8', image_name: '', answer2: '8', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 7, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 8, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''}
+];
