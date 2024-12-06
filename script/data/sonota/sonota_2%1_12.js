8695c85d6bd0c71c73b4faa7f746609f31f95ca7
mochimochi315 <mochi213we@yahoo.co.jp>
2024-12-06 10:08:48 +0900

    Delete script/data/sonota/sonota_2%1_12.js

--- script/data/sonota/sonota_2%1_12.js
+++
@@ -1,9 +0,0 @@
-const mondai = [
-    { number: 1, question: '6÷3＝', answer: '2', image_name: '', answer2: '2', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 2, question: '8÷2＝', answer: '4', image_name: '', answer2: '4', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 3, question: '6÷1＝', answer: '6', image_name: '', answer2: '6', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 4, question: '63÷9＝', answer: '7', image_name: '', answer2: '7', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 5, question: '72÷8＝', answer: '9', image_name: '', answer2: '9', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 6, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 7, question: '', answer: '', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''}
-];
