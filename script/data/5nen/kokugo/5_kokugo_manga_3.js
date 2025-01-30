514101cb1d9dea173ee112a384c73237d68a390f
mochimochi315 <mochi213we@yahoo.co.jp>
2025-01-30 11:28:47 +0900

    Add files via upload

--- script/data/5nen/kokugo/5_kokugo_manga_3.js
+++ script/data/5nen/kokugo/5_kokugo_manga_3.js
@@ -1,6 +1,6 @@
 const mondai = [
     { number: 1, question: 'P90～P91　「一コマまんが」や「四コマまんが」「スリーリーまんが」は、すべてまんがの仲間とされています。それはなぜですか。<BR>それは、まんがに特有の、（　　　）が見られるから。', answer: 'きょうつうしたひょうげんほうほう', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
-    { number: 2, question: 'P91　ストーリーまんがは、「（　　　）」とよばれる四角いわくの中にえがかれた絵を連続させて、表現されます。（カタカナ）', answer: 'コマ', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: 'P92　ストーリーまんがは、「（　　　）」とよばれる四角いわくの中にえがかれた絵を連続させて、表現されます。（カタカナ）', answer: 'コマ', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 3, question: 'P92　大きなコマや変わった形のコマが入ると、（　　　）が強まります。', answer: 'ばめんのいんしょう', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 4, question: 'P92　数種類のコマを組み合わせて、回想や想像、夢など、時間や（　　　）を表現することもあります。', answer: 'こころのうごき', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
     { number: 5, question: 'P92　そのせりふがどの人物のものなのかは、人物とフキダシとの（　　　）からわかります', answer: 'いちかんけい', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
