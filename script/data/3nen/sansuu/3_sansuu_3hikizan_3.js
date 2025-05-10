1a510c4fd9ca276bce8bc9dfe45b788298b58c33
mochimochi315 <mochi213we@yahoo.co.jp>
2025-05-10 18:46:45 +0900

    Add files via upload

---
+++ script/data/3nen/sansuu/3_sansuu_3hikizan_3.js
@@ -0,0 +1,12 @@
+const mondai = [
+    { number: 1, question: '922 − 256 =', answer: '666', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 2, question: '602 − 395 =', answer: '207', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 3, question: '815 − 239 =', answer: '576', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 4, question: '851 − 494 =', answer: '357', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 5, question: '720 − 443 =', answer: '277', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 6, question: '637 − 259 =', answer: '378', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 7, question: '531 − 364 =', answer: '167', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 8, question: '853 − 468 =', answer: '385', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 9, question: '902 − 436 =', answer: '466', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''},
+    { number: 10, question: '721 − 248 =', answer: '473', image_name: '', answer2: '', etc_2: '', etc_3: '', etc_4: ''}
+];
